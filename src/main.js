5025f1f91b118d70c792541421857deaeba111ad
liufafa <15311477565@163.com>
2024-03-21 09:53:47 +0800

    ceshi

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
 import './assets/main.css'
+
+import Antd from "ant-design-vue"
+import  "ant-design-vue/dist/antd."
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
